69fe26e141ce8385f96e97af6c433a0bfb65ecf8
Marco Nikander <m4n1k.developer@gmail.com>
2025-11-24 21:08:00 +0100

    tag all the types in the cons-cell prototype and test a list

--- 0006-cons-cells/cons-cells.test.ts
+++ 0006-cons-cells/cons-cells.test.ts
@@ -1,29 +1,39 @@
 import { describe, it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
-import { first, rest, cons, Cons } from "./cons-cells.ts";
+import { nil, first, rest, cons, Cons, atom } from "./cons-cells.ts";
 
 describe('must support the construction and access of a pair of', () => {
     it('booleans', () => {
-        const pair: Cons = cons(false, true);
-        expect(first(pair)).toBe(false);
-        expect(rest(pair)).toBe(true);
+        const pair: Cons = cons(atom(false), atom(true));
+        expect(first(pair)).toEqual(atom(false));
+        expect(rest(pair)).toEqual(atom(true));
     });
 
     it('integers', () => {
-        const pair: Cons = cons(1, 2);
-        expect(first(pair)).toBe(1);
-        expect(rest(pair)).toBe(2);
+        const pair: Cons = cons(atom(1), atom(2));
+        expect(first(pair)).toEqual(atom(1));
+        expect(rest(pair)).toEqual(atom(2));
     });
 
-    it('undefined', () => {
-        const pair: Cons = cons(undefined, undefined);
-        expect(first(pair)).toBe(undefined);
-        expect(rest(pair)).toBe(undefined);
+    it('nil', () => {
+        const pair: Cons = cons(nil, nil);
+        expect(first(pair)).toEqual(nil);
+        expect(rest(pair)).toEqual(nil);
     });
 
-    it('integer and undefined', () => {
-        const pair: Cons = cons(42, undefined);
-        expect(first(pair)).toBe(42);
-        expect(rest(pair)).toBe(undefined);
+    it('integer and nil', () => {
+        const pair: Cons = cons(atom(42), nil);
+        expect(first(pair)).toEqual(atom(42));
+        expect(rest(pair)).toEqual(nil);
+    });
+});
+
+describe('must support the construction and access of a list', () => {
+    it('integers', () => {
+        const list: Cons = cons(atom(1), cons(atom(2), cons(atom(3), nil)));
+        expect(first(list)).toEqual(atom(1));
+        expect(first(rest(list))).toEqual(atom(2));
+        expect(first(rest(rest(list)))).toEqual(atom(3));
+        expect(rest(rest(rest(list)))).toEqual(nil);
     });
 });
--- 0006-cons-cells/cons-cells.ts
+++ 0006-cons-cells/cons-cells.ts
@@ -1,17 +1,29 @@
 // Copyright (c) 2025 Marco Nikander
 
-export type Value = undefined | boolean | number | Cons;
+export type Value = Nil | Atom | Cons;
+export type Nil   = { tag: 'Nil' };
+export type Atom  = { tag: 'Atom', value: boolean | number };
 export type Cons  = { tag: 'Cons', left: Value, right: Value };
+
+export const nil: Nil = { tag: 'Nil' };
+
+export function atom(value: boolean | number): Atom {
+    return { tag: 'Atom', value: value };
+}
 
 export function cons(left: Value, right: Value): Cons {
     return { tag: 'Cons', left: left, right: right };
 }
 
-export function first(cons: Cons): Value {
-    return cons.left;
+export function first(cons: Value): Value {
+    return assert_cons(cons, 'first').left;
 }
 
-export function rest(cons: Cons): Value {
-    return cons.right;
+export function rest(cons: Value): Value {
+    return assert_cons(cons, 'rest').right;
 }
 
+function assert_cons(v: Value, name: string): Cons {
+    if (v.tag !== 'Cons') throw Error(`'${name}' expects 'x' to be a Cons-Cell, got ${v.tag} instead.`);
+    else return v;
+}
