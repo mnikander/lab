9d90e5843dbcee9fb584a34d84acca595d2aad9b
Marco Nikander <m4n1k.developer@gmail.com>
2025-11-24 21:13:47 +0100

    test tree structure

--- 0006-cons-cells/cons-cells.test.ts
+++ 0006-cons-cells/cons-cells.test.ts
@@ -28,12 +28,20 @@ describe('must support the construction and access of a pair of', () => {
     });
 });
 
-describe('must support the construction and access of a list', () => {
-    it('integers', () => {
+describe('must support the construction and access of', () => {
+    it('a list of integers', () => {
         const list: Cons = cons(atom(1), cons(atom(2), cons(atom(3), nil)));
         expect(first(list)).toEqual(atom(1));
         expect(first(rest(list))).toEqual(atom(2));
         expect(first(rest(rest(list)))).toEqual(atom(3));
         expect(rest(rest(rest(list)))).toEqual(nil);
+    });
+
+    it('a tree of integers', () => {
+        const list: Cons = cons(cons(atom(1), atom(2)), cons(atom(3), nil));
+        expect(first(first(list))).toEqual(atom(1));
+        expect(rest(first(list))).toEqual(atom(2));
+        expect(first(rest(list))).toEqual(atom(3));
+        expect(rest(rest(list))).toEqual(nil);
     });
 });
