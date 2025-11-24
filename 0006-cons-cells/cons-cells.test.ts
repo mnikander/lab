9b5981ebfeb05a410643623fe2140cb55b24f3ca
Marco Nikander <m4n1k.developer@gmail.com>
2025-11-24 20:48:54 +0100

    start implementing basic cons cells

--- 0006-cons-cells/cons-cells.test.ts
+++ 0006-cons-cells/cons-cells.test.ts
@@ -1,8 +1,29 @@
 import { describe, it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
+import { first, rest, cons, Cons } from "./cons-cells.ts";
 
-describe('basic', () => {
-    it('tautology', () => {
-        expect(true).toBe(true);
+describe('must support the construction and access of a pair of', () => {
+    it('booleans', () => {
+        const pair: Cons = cons(false, true);
+        expect(first(pair)).toBe(false);
+        expect(rest(pair)).toBe(true);
+    });
+
+    it('integers', () => {
+        const pair: Cons = cons(1, 2);
+        expect(first(pair)).toBe(1);
+        expect(rest(pair)).toBe(2);
+    });
+
+    it('undefined', () => {
+        const pair: Cons = cons(undefined, undefined);
+        expect(first(pair)).toBe(undefined);
+        expect(rest(pair)).toBe(undefined);
+    });
+
+    it('integer and undefined', () => {
+        const pair: Cons = cons(42, undefined);
+        expect(first(pair)).toBe(42);
+        expect(rest(pair)).toBe(undefined);
     });
 });
--- 0006-cons-cells/cons-cells.ts
+++ 0006-cons-cells/cons-cells.ts
@@ -1,2 +1,17 @@
 // Copyright (c) 2025 Marco Nikander
 
+export type Value = undefined | boolean | number | Cons;
+export type Cons  = { tag: 'Cons', left: Value, right: Value };
+
+export function cons(left: Value, right: Value): Cons {
+    return { tag: 'Cons', left: left, right: right };
+}
+
+export function first(cons: Cons): Value {
+    return cons.left;
+}
+
+export function rest(cons: Cons): Value {
+    return cons.right;
+}
+
