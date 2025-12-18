905720dcf34987a1d13e3ea4feb8a60a9fbe8858
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-18 19:46:20 +0100

    attempt to find parallel branches in a DAG

--- 0007-datalog-racket/dominance_frontier.rkt
+++ 0007-datalog-racket/dominance_frontier.rkt
@@ -11,9 +11,23 @@ edge(a, b).
 predecessor(X, Z) :- edge(X, Z).
 predecessor(X, Z) :- edge(X, Y), predecessor(Y, Z).
 
+inclusive_predecessor(X, Z) :- X = Z.
+inclusive_predecessor(X, Z) :- edge(X, Z).
+inclusive_predecessor(X, Z) :- edge(X, Y), predecessor(Y, Z).
+
 descendents(Ancestor, L, R) :- predecessor(Ancestor, L), predecessor(Ancestor, R).
 ancestors(L, R, Descendent) :- predecessor(L, Descendent), predecessor(R, Descendent).
 
-split(L, R) :- edge(P, L), edge(P, R), L != R.
+split(X, Y)   :- edge(P, X), edge(P, Y), X != Y.
 
-split(b, X)?
+cousins(X, Y) :- 
+    edge(O, L), 
+    edge(O, R),
+    predecessor(O, X), 
+    predecessor(O, Y),
+    inclusive_predecessor(L, X),
+    inclusive_predecessor(R, Y),
+    L != R,
+    X != Y.
+
+cousins(X, Y)?
