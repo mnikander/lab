77d0ecd4d6e7db9c0700dfb516857710d7b9609a
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-19 17:52:34 +0100

    try carrying the value through the CFG

--- 0008-datalog-exercises/4-data-flow-analysis.rkt
+++ 0008-datalog-exercises/4-data-flow-analysis.rkt
@@ -36,6 +36,26 @@ define(d1, b2).
 undefine(d1, b4).
 define(d2, b4).
 
-reachable(D, B) :- define(D, B).
+%path(X, X) :- edge(X, Y). % path length 0, the node itself
+path(X, Y) :- edge(X, Y).
+path(X, Z) :- edge(X, Y), path(Y, Z).
 
-reachable(D, X)?
+% I think most of these things only work when there is exactly one define/undefine in the entire graph:
+
+% these two relations express which definitions can and cannot flow along a particular edge
+flow(D, X, Y) :- edge(X, Y), undefine(D, U), U != Y.
+stop(D, X, Y) :- edge(X, Y), undefine(D, Y).
+
+carries(D, X, Y) :- define(D, X), flow(D, X, Y).                   % Carries_1
+carries(D, X, Z) :- define(D, X), flow(D, X, Y), carries(D, Y, Z). % Carries_2
+%       d2 b4 b3          d2  b4       d2 b4 b1          d2 b1 b3  
+% the last condition in Carries_2 fails because the Carries_1 requires d2 to be defined in b1, which is false
+
+variable(D, BLK) :- define(D, BLK).
+variable(D, BLK) :- edge(PRED, BLK), define(D, PRED), undefine(D, U), U != BLK.
+
+%path(X, b3)?
+%variable(D, B)?
+flow(D, X, Y)?
+stop(D, X, Y)?
+carries(D, X, Y)?
