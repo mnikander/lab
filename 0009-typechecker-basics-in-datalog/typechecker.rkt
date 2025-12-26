e7b5a0370a4a330a3d547d0b79bc1794015714dd
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-26 20:06:16 +0100

    improve comments for the type-checker

--- 0009-typechecker-basics-in-datalog/typechecker.rkt
+++ 0009-typechecker-basics-in-datalog/typechecker.rkt
@@ -1,10 +1,10 @@
 #lang datalog
 
-% definition of type i1, i.e. booleans
+% Definition of type i1, i.e. booleans
 in(0, i1).
 in(1, i1).
 
-% definition of type i4
+% Definition of type i4
 in(0, i4).
 in(1, i4).
 in(2, i4).
@@ -14,22 +14,31 @@ in(5, i4).
 in(6, i4).
 in(7, i4).
 
-% helpers
+% Helpers
 assert_constant(L, T)    :- constant(L, V), in(V, T).
 assert_variable(L, N, T) :- variable(L, N), type(N, T).
 assert_call(L, T)        :- call(L, F), signature(F, T_other, T), assert_arg(L, V, T_other).
 assert_arg(L, V, T)      :- arg(L, V), in(V, T).
 assert_arg(L, N, T)      :- arg(L, N), type(N, T).
 
-% relations
+% Relations
 type(N, T)               :- let(L, N, T), assert_constant(L, T).
 type(N, T)               :- let(L, N, T), assert_variable(L, N_other, T).
 type(N, T)               :- let(L, N, T), assert_call(L, T).
 
-% function signatures
-signature(not, i1, i1).
+% Function signatures
+signature(not, i1, i1).                      % not :: i1 -> i1
 
-% code
+% Code has the form:
+%
+%    let a: i1 = 0
+%
+% Datalog does not care about the order of statements. Each statement ends with
+% dot '.'. In the statements below, the leading index is always the source line
+% number. Statements for the let-binding on the left-hand side of the equals
+% sign, and the constant, variable access, function call, and parameters on the
+% right-hand side, are all stitched together using their source line number.
+
 let( 0, a, i1). constant( 0, 0).              % ok
 let( 1, b, i1). constant( 1, 1).              % ok
 let( 2, c, i1). constant( 2, 2).              % error: trying to assign an i4 into an i1
@@ -44,5 +53,5 @@ let(10, k, i1). call(10, not). arg(10, d).    % error: 'not' expects an i1
 let(11, l, i1). call(11, not). arg(11, c).    % error: trying to assign from a variable which has an error
 let(12, m, i4). call(12, not). arg(12, 0).    % error: trying to assign an i1 into an i4
 
-% queries
+% Query to output all variables with a valid type
 type(N, T)?
