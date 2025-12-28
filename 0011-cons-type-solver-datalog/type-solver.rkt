7d50e035cb8712682a555d85436babbc47f19e3b
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-28 21:06:10 +0100

    0011: WIP multi-argument functions

--- 0011-cons-type-solver-datalog/type-solver.rkt
+++ 0011-cons-type-solver-datalog/type-solver.rkt
@@ -36,22 +36,52 @@ not_equal(L, R)   :- cons(L, L1, L2), cons(R, R1, R2), not_equal(L2, R2), L != R
 %
 % Note that all ids are assumed to be unique. This must be ensured beforehand.
 
+check_inputs (Args, Sig) :- cons(Sig, In, Out), in(Args, In).
 check_inputs (Args, Sig) :- cons(Sig, In, Out), atom(Args, V), in(V, In).
+% something is broken here:                                                           v v v v v v v v v v v v v v v v v v v v v
+check_inputs (Args, Sig) :- cons(Sig, In, Out), cons(Args, V1, V2), cons(In, T1, T2), check_inputs(V1, T1), check_inputs(V2, T2).
 check_outputs(Type, Sig) :- cons(Sig, In, Out), equal(Type, Out).
 
 typeof(Id, Type) :- atom(Id, Value), type(Id, Type), in(Value, Type).
 typeof(Id, Type) :- func(Id), type(Id, Type), cons(Type, T1, T2).
-typeof(Id, Type) :- call(Id, Fn, Args), func(Fn), type(Fn, Sig), check_inputs(Args, Sig), check_outputs(Type, Sig).
-
-% Define several atoms
-atom(a,   0). type(a, i1).    % ok
-atom(b, nil). type(b, i1).    % error
-atom(c, 666). type(c, i1).    % error
+typeof(Id, Type) :- call(Id, Fn, Args), type(Id, Type), func(Fn), type(Fn, Sig), check_inputs(Args, Sig), check_outputs(Type, Sig).
 
 % function flip : i1 -> i1
 func(flip). type(flip, sig_flip). cons(sig_flip, i1, i1).
 
-atom(d, i1). call(d, flip, a).
+% function mix : i1 -> i1 -> i1
+func(mix). type(mix, sig_mix). cons(sig_mix, sig_mix_input, i1). cons(sig_mix_input, i1, i1).
+
+% Define atoms and cons-cells
+atom(a, 0).
+type(a, i1).               % ok
+
+atom(b, 1).
+type(b, i1).               % ok
+
+atom(c, nil).
+type(c, i1).               % error
+
+atom(d, 666).
+type(d, i1).               % error
+
+type(e, i1).
+call(e, flip, a).          % ok
+
+type(f, i1).
+call(f, flip, b).          % ok
+
+type(g, i4).
+call(g, flip, a).          % error
+
+cons(zero-one, 0, 1).
+cons(a-b, a, b).
+
+type(h, i1).
+call(h, mix, zero-one).    % ok -- something is broken here
+
+type(i, i1).
+call(i, mix, a-b).         % ok -- something is broken here
 
 % Query
 typeof(X, Type)?
