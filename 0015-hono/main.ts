0c11ea82427c3280510c8a11b17ace009be9038b
Marco Nikander <m4n1k.developer@gmail.com>
2026-03-16 21:53:25 +0100

    0015 functions to load and save data

--- 0015-hono/main.ts
+++ 0015-hono/main.ts
@@ -1,5 +1,24 @@
 import { Hono } from 'hono'
 
+type Entry = { year: number, month: number, day: number, total: number};
+
+const file = "./data.json";
+
+async function load(filename: string) {
+  try {
+    const text = await Deno.readTextFile(filename);
+    return JSON.parse(text);
+  }
+  catch {
+    return [];
+  }
+}
+
+async function save(data: Entry[], filename: string) {
+  const text = JSON.stringify(data, null, 2);
+  await Deno.writeTextFile(filename, text);
+}
+
 const app = new Hono();
 
 app.get('/', (c) => {
@@ -7,9 +26,7 @@ app.get('/', (c) => {
 });
 
 app.get('/data', async (c) => {
-  const text = await Deno.readTextFile("./data.json");
-  const data = JSON.parse(text);
-  return c.json(data);
+  return c.json(await load(file));
 });
 
 Deno.serve(app.fetch);
