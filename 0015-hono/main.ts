05007d9b6d9b21030d83ab6a23f796e70262ff11
Marco Nikander <m4n1k.developer@gmail.com>
2026-03-16 22:14:31 +0100

    0015 post request with basic validation

--- 0015-hono/main.ts
+++ 0015-hono/main.ts
@@ -29,4 +29,32 @@ app.get('/data', async (c) => {
   return c.json(await load(file));
 });
 
+app.post('/data', async (c) => {
+  const entry = await c.req.json();
+  if ("year" in entry
+    && "month" in entry
+    && "day" in entry
+    && "total" in entry
+    && typeof entry.year === "number"
+    && typeof entry.month === "number"
+    && typeof entry.day === "number"
+    && typeof entry.total === "number"
+    && entry.year > 2000
+    && entry.year <= 2030
+    && entry.month > 0
+    && entry.month <= 12
+    && entry.day > 0
+    && entry.day <= 31
+    && entry.total > 0
+) {
+        const data = await load(file);
+        data.push(entry);
+        await save(data, file);
+        return c.json({ success: true });
+    }
+    else {
+        return c.json({ error: "invalid data" }, 400);
+    }
+});
+
 Deno.serve(app.fetch);
