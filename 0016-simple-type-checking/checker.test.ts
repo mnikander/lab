72ae4d2d2cf57c6d78045d97478c96c0cecda6de
Marco Nikander <m4n1k.developer@gmail.com>
2026-03-22 19:00:31 +0100

    0016 expand and rephrase type-checker tests

--- 0016-simple-type-checking/checker.test.ts
+++ 0016-simple-type-checking/checker.test.ts
@@ -3,66 +3,84 @@ import { expect } from "jsr:@std/expect@1.0.17";
 import { check_application, check_assignment } from "./checker.ts";
 import { Arrow, Simple, Type } from "./types.ts";
 
-describe('check assignment', () => {
-    it('Bool__assign__Bool', () => {
+describe('assignment of simple types which are', () => {
+    it('identical must type check', () => {
         const left: Simple  = ["Bool"] ;
         const right: Simple = ["Bool"] ;
         expect(check_assignment(left, right)).toEqual(true);
     });
 
-    it('Bool__assign__Int64', () => {
+    it('different must NOT type check', () => {
         const left: Simple  = ["Bool"] ;
         const right: Simple = ["Int64"] ;
         expect(check_assignment(left, right)).toEqual(false);
     });
 });
 
-describe('check first-order functions', () => {
-    it('Int64_to_Bool__applied__Int64', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Bool"] ];
-        const arg: Simple[] = [["Int64"]];
+describe('application of first-order functions to', () => {
+    it('matching arguments must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Bool"] ];
+        const arg: Type[] = [["Int64"]];
         expect(check_application(fn, arg)).toEqual(true);
     });
 
-    it('Int64_to_VariantBoolInt64__applied__Int64', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Variant", [["Bool"], ["Int64"]]]];
-        const arg: Simple[] = [["Int64"]];
+    it('mismatching argument must NOT type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Bool"] ];
+        const arg: Type[] = [["Char8"]];
+        expect(check_application(fn, arg)).toEqual(false);
+    });
+
+    it('missing arguments must NOT type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Bool"] ];
+        const arg: Type[] = [];
+        expect(check_application(fn, arg)).toEqual(false);
+    });
+
+    it('excess arguments must NOT type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Bool"] ];
+        const arg: Type[] = [["Int64"], ["Int64"]];
+        expect(check_application(fn, arg)).toEqual(false);
+    });
+
+    it('matching binary arguments must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Arrow", ["Int64"], ["Bool"] ] ];
+        const arg: Type[] = [["Int64"], ["Int64"]];
         expect(check_application(fn, arg)).toEqual(true);
+    });
+
+    it('mismatching binary arguments must NOT type check', () => {
+        const fn: Arrow   = ["Arrow", ["Int64"], ["Arrow", ["Int64"], ["Bool"] ] ];
+        const arg: Type[] = [["Int64"], ["Bool"]];
+        expect(check_application(fn, arg)).toEqual(false);
+    });
+
+    it('matching variants must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Variant", [["Bool"], ["Int64"]]], ["Int64"]];
+        const arg: Type[] = [["Variant", [["Bool"], ["Int64"]]]];
+        expect(check_application(fn, arg)).toEqual(true);
+    });
+
+    it('mismatching variants must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Variant", [["Bool"], ["Int64"]]], ["Int64"]];
+        const arg: Type[] = [["Variant", [["Unit"], ["Top"]]]];
+        expect(check_application(fn, arg)).toEqual(false);
     });
     
-    it('Int64_to_TupleBoolInt64__applied__Int64', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Tuple", [["Bool"], ["Int64"]]]];
-        const arg: Simple[] = [["Int64"]];
+    it('matching tuples must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Tuple", [["Bool"], ["Int64"]]], ["Int64"]];
+        const arg: Type[] = [["Tuple", [["Bool"], ["Int64"]]]];
         expect(check_application(fn, arg)).toEqual(true);
     });
 
-    it('Int64_to_Bool__applied__no_arguments', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Bool"] ];
-        const arg: Simple[] = [];
-        expect(check_application(fn, arg)).toEqual(false);
-    });
-
-    it('Int64_to_Bool__applied__Char8', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Bool"] ];
-        const arg: Simple[] = [["Char8"]];
-        expect(check_application(fn, arg)).toEqual(false);
-    });
-
-    it('Int64_to_Int64_to_Bool__applied__Int64_Int64', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Arrow", ["Int64"], ["Bool"] ] ];
-        const arg: Simple[] = [["Int64"], ["Int64"]];
-        expect(check_application(fn, arg)).toEqual(true);
-    });
-
-    it('Int64_to_Int64_to_Bool__applied__Int64_Bool', () => {
-        const fn: Arrow     = ["Arrow", ["Int64"], ["Arrow", ["Int64"], ["Bool"] ] ];
-        const arg: Simple[] = [["Int64"], ["Bool"]];
+    it('mismatching tuples must type check', () => {
+        const fn: Arrow   = ["Arrow", ["Tuple", [["Bool"], ["Int64"]]], ["Int64"]];
+        const arg: Type[] = [["Tuple", [["Unit"], ["Top"]]]];
         expect(check_application(fn, arg)).toEqual(false);
     });
 });
 
 describe('check higher-order functions', () => {
-    it('Int64toBool_to_Int64_to_Bool__applied__Int64', () => {
+    it('matching function arguments and simple arguments must type check', () => {
         // this is a higher-order function like 'map', but on one element:
         // fn : (Int64 -> Bool) -> Int64 -> Bool
         // note that arrows are right-associative, to this is equivalent to:
@@ -72,7 +90,7 @@ describe('check higher-order functions', () => {
         expect(check_application(fn, arg)).toEqual(true);
     });
 
-        it('Int64toBool_to_Int64_to_Bool__applied__Int64', () => {
+        it('matching simple arguments and function arguments must type check', () => {
         // this is a higher-order function like 'map', but on one element:
         // fn : Int64 -> (Int64 -> Bool) -> Bool
         // note that arrows are right-associative, to this is equivalent to:
