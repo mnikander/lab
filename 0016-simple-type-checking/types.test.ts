9748af1f0ae8c65d921ef6ac23cdee1f38f1d29a
Marco Nikander <m4n1k.developer@gmail.com>
2026-03-22 18:31:04 +0100

    0016 sum and product types

--- 0016-simple-type-checking/types.test.ts
+++ 0016-simple-type-checking/types.test.ts
@@ -2,7 +2,7 @@ import { describe, it } from "jsr:@std/testing@1.0.16/bdd";
 import { expect } from "jsr:@std/expect@1.0.17";
 import { equivalent, Type } from "./types.ts"
 
-describe('type equivalence', () => {
+describe('simple type equivalence', () => {
     it('Bool__equal__Bool', () => {
         const a: Type = ["Bool"];
         const b: Type = ["Bool"];
@@ -43,5 +43,93 @@ describe('type equivalence', () => {
         const a: Type = [ "Arrow", ["Bool"], [ "Arrow", ["Bool"], ["Bool"] ] ];
         const b: Type = [ "Arrow", ["Bool"], [ "Arrow", ["Bool"], ["Bool"] ] ];
         expect(equivalent(a, b)).toBe(true);
+    });
+});
+
+describe('sum-type equivalence', () => {
+    it('empty variants must be equivalent', () => {
+        const a: Type = ["Variant", []];
+        const b: Type = ["Variant", []];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('identical unary variants must be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"]]];
+        const b: Type = ["Variant", [["Bool"]]];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('different unary variants must NOT be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"]]];
+        const b: Type = ["Variant", [["Int64"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+    
+    it('identical binary variants must be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"], ["Int64"]]];
+        const b: Type = ["Variant", [["Bool"], ["Int64"]]];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('reversed binary variants, must be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"], ["Int64"]]];
+        const b: Type = ["Variant", [["Int64"], ["Bool"]]];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('different binary variants must NOT be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"], ["Int64"]]];
+        const b: Type = ["Variant", [["Bool"], ["Char8"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+
+    it('different-size variants, must NOT be equivalent', () => {
+        const a: Type = ["Variant", [["Bool"]]];
+        const b: Type = ["Variant", [["Bool"], ["Int64"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+});
+
+describe('product-type equivalence', () => {
+    it('empty tuples must be equivalent', () => {
+        const a: Type = ["Tuple", []];
+        const b: Type = ["Tuple", []];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('identical unary tuples must be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"]]];
+        const b: Type = ["Tuple", [["Bool"]]];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('different unary tuples must NOT be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"]]];
+        const b: Type = ["Tuple", [["Int64"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+
+    it('identical binary tuples must be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"], ["Int64"]]];
+        const b: Type = ["Tuple", [["Bool"], ["Int64"]]];
+        expect(equivalent(a, b)).toBe(true);
+    });
+
+    it('reversed binary tuples, must NOT be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"], ["Int64"]]];
+        const b: Type = ["Tuple", [["Int64"], ["Bool"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+
+    it('different binary tuples must NOT be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"], ["Int64"]]];
+        const b: Type = ["Tuple", [["Bool"], ["Char8"]]];
+        expect(equivalent(a, b)).toBe(false);
+    });
+
+    it('different-size tuples, must NOT be equivalent', () => {
+        const a: Type = ["Tuple", [["Bool"]]];
+        const b: Type = ["Tuple", [["Bool"], ["Int64"]]];
+        expect(equivalent(a, b)).toBe(false);
     });
 });
