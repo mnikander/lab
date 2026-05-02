bfcc938f49a9536995b253f0d2695e0301ceb36d
Marco Nikander <m4n1k.developer@gmail.com>
2026-05-02 11:05:44 +0200

    0017 define tuple-based grammar

--- 0017-anf-inspired-grammar/tuple_grammar.test.ts
+++ 0017-anf-inspired-grammar/tuple_grammar.test.ts
@@ -1,8 +1,46 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
+import * as Grammar from "./tuple_grammar.ts";
 
-describe('tuple-based grammar', () => {
-    it('tautology', () => {
-        expect(true).toEqual(true);
-    });
+describe("tuple-based grammar", () => {
+  it("must allow an empty block", () => {
+    const _text: string = "{unit}";
+    const input: Grammar.Block = ["block", [], ["unit"]];
+    expect(input).toBeDefined();
+  });
+
+  it("must allow simple arithmetic expressions", () => {
+    const _text: string = "{+ 1 2}";
+    const input: Grammar.Block = ["block", [], ["binary", "+", [["int", 1], [
+      "int",
+      2,
+    ]]]];
+    expect(input).toBeDefined();
+  });
+
+  it("must allow nested arithmetic expressions", () => {
+    const _text: string = "{+ 1 {+ 2 3}}";
+    const input: Grammar.Block = ["block", [], ["binary", "+", [["int", 1], [
+      "block",
+      [],
+      ["binary", "+", [["int", 2], ["int", 3]]],
+    ]]]];
+    expect(input).toBeDefined();
+  });
+
+  it("must allow arithmetic let-bindings", () => {
+    const _text: string = "{+ 1 { let x = + 2 3 in x }}";
+    const input: Grammar.Block = [
+      "block",
+      [],
+      ["binary", "+", [["int", 1], [
+        "block",
+        [
+          ["let", ["id", "x"], ["binary", "+", [["int", 2], ["int", 3]]]],
+        ],
+        ["id", "x"],
+      ]]],
+    ];
+    expect(input).toBeDefined();
+  });
 });
--- 0017-anf-inspired-grammar/tuple_grammar.ts
+++ 0017-anf-inspired-grammar/tuple_grammar.ts
@@ -1,1 +1,41 @@
 // Copyright (c) 2026 Marco Nikander
+
+export type Block = ["block", Let[], Tail];
+export type Let = ["let", variable: Id, value: Expression];
+export type Tail = Expression | Conditional;
+
+export type Expression = Atom | Application;
+export type Atom =
+  | Unit
+  | Int
+  | Id
+  | Builtin
+  | Abstraction
+  | Application
+  | Block;
+
+// TODO: 'Abstraction' is very similar to the block definition. Can I use functions at the top level and get rid of the Block?
+export type Abstraction = ["abstraction", Id[], Block];
+export type Application = ["application", Atom, Expression[]];
+
+export type Conditional = [
+  "if",
+  cond: Expression,
+  thenExp: Expression,
+  elseExp: Expression,
+];
+
+export type Builtin = Unary | Binary;
+export type Unary = ["unary", op: Op_unary, args: [Expression]];
+export type Binary = [
+  "binary",
+  op: Op_arithmetic | Op_comparison,
+  args: [Expression, Expression],
+];
+
+export type Op_unary = "~";
+export type Op_arithmetic = "+" | "-" | "*" | "/" | "%" | "min" | "max";
+export type Op_comparison = "<" | ">" | "<=" | ">=" | "==" | "!=";
+export type Id = ["id", string];
+export type Int = ["int", number];
+export type Unit = ["unit"];
