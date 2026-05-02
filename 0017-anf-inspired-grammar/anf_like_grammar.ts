b241d55843e113c52bfbe23b6b6467bc3a34b2ec
Marco Nikander <m4n1k.developer@gmail.com>
2026-05-02 14:50:40 +0200

    0017: refactor ANF-like grammar

--- 0017-anf-inspired-grammar/anf_like_grammar.ts
+++ 0017-anf-inspired-grammar/anf_like_grammar.ts
@@ -6,17 +6,17 @@ export type Tail = Expression | Conditional;
 
 export type Expression = Atom | Application;
 export type Atom =
-  | Unit
-  | Int
-  | Id
-  | Builtin
-  | Abstraction
+  | Block
   | Application
-  | Block;
+  | Abstraction
+  | Builtin
+  | Id
+  | Int
+  | Unit;
 
 // TODO: 'Abstraction' is very similar to the block definition. Can I use functions at the top level and get rid of the Block?
-export type Abstraction = ["abstraction", Id[], Block];
-export type Application = ["application", Atom, Expression[]];
+export type Application = ["call", Atom, Expression[]];
+export type Abstraction = ["func", Id[], Block];
 
 export type Conditional = [
   "if",
@@ -27,13 +27,10 @@ export type Conditional = [
 
 export type Builtin = Unary | Binary;
 export type Unary = ["unary", op: Op_unary, args: [Expression]];
-export type Binary = [
-  "binary",
-  op: Op_arithmetic | Op_comparison,
-  args: [Expression, Expression],
-];
+export type Binary = ["binary", op: Op_binary, args: [Expression, Expression]];
 
 export type Op_unary = "~";
+export type Op_binary = Op_arithmetic | Op_comparison;
 export type Op_arithmetic = "+" | "-" | "*" | "/" | "%" | "min" | "max";
 export type Op_comparison = "<" | ">" | "<=" | ">=" | "==" | "!=";
 export type Id = ["id", string];
