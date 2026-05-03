b3c568357299e9f4586f3ee1ded9ca523048812d
Marco Nikander <m4n1k.developer@gmail.com>
2026-05-03 12:48:39 +0200

    0018 improve test descriptions

--- 0018-lifecycle-grammar/lifecycle.test.ts
+++ 0018-lifecycle-grammar/lifecycle.test.ts
@@ -3,235 +3,235 @@ import { expect } from "@std/expect";
 import { define, free, get_state, is_ok, Result, use } from "./lattice.ts";
 
 describe("life-cycle", () => {
-  it("define", () => {
+  it("(define) => ok", () => {
     const result: Result = define(["ok", "pre"]);
     expect(is_ok(result)).toBe(true);
     expect(get_state(result)).toEqual("live");
   });
 
-  it("use", () => {
+  it("(use) => error", () => {
     const result: Result = use(["ok", "pre"]);
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free", () => {
+  it("(free) => error", () => {
     const result: Result = free(["ok", "pre"]);
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, define", () => {
+  it("(define, define) => error", () => {
     const result: Result = define(define(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, use", () => {
+  it("(define, use) => ok", () => {
     const result: Result = use(define(["ok", "pre"]));
     expect(is_ok(result)).toBe(true);
     expect(get_state(result)).toEqual("live");
   });
 
-  it("define, free", () => {
+  it("(define, free) => ok", () => {
     const result: Result = free(define(["ok", "pre"]));
     expect(is_ok(result)).toBe(true);
     expect(get_state(result)).toEqual("post");
   });
 
-  it("use, define", () => {
+  it("(use, define) => error", () => {
     const result: Result = define(use(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, use", () => {
+  it("(use, use) => error", () => {
     const result: Result = use(use(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, free", () => {
+  it("(use, free) => error", () => {
     const result: Result = free(use(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, define", () => {
+  it("(free, define) => error", () => {
     const result: Result = define(free(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, use", () => {
+  it("(free, use) => error", () => {
     const result: Result = use(free(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, free", () => {
+  it("(free, free) => error", () => {
     const result: Result = free(free(["ok", "pre"]));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, define, define", () => {
+  it("(define, define, define) => error", () => {
     const result: Result = define(define(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, define, use", () => {
+  it("(define, define, use) => error", () => {
     const result: Result = use(define(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, define, free", () => {
+  it("(define, define, free) => error", () => {
     const result: Result = free(define(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, use, define", () => {
+  it("(define, use, define) => error", () => {
     const result: Result = define(use(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, use, use", () => {
+  it("(define, use, use) => ok", () => {
     const result: Result = use(use(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(true);
     expect(get_state(result)).toEqual("live");
   });
 
-  it("define, use, free", () => {
+  it("(define, use, free) => ok", () => {
     const result: Result = free(use(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(true);
     expect(get_state(result)).toEqual("post");
   });
 
-  it("define, free, define", () => {
+  it("(define, free, define) => error", () => {
     const result: Result = define(free(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, free, use", () => {
+  it("(define, free, use) => error", () => {
     const result: Result = use(free(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("define, free, free", () => {
+  it("(define, free, free) => error", () => {
     const result: Result = free(free(define(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, define, define", () => {
+  it("(use, define, define) => error", () => {
     const result: Result = define(define(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, define, use", () => {
+  it("(use, define, use) => error", () => {
     const result: Result = use(define(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, define, free", () => {
+  it("(use, define, free) => error", () => {
     const result: Result = free(define(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, use, define", () => {
+  it("(use, use, define) => error", () => {
     const result: Result = define(use(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, use, use", () => {
+  it("(use, use, use) => error", () => {
     const result: Result = use(use(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, use, free", () => {
+  it("(use, use, free) => error", () => {
     const result: Result = free(use(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, free, define", () => {
+  it("(use, free, define) => error", () => {
     const result: Result = define(free(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, free, use", () => {
+  it("(use, free, use) => error", () => {
     const result: Result = use(free(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("use, free, free", () => {
+  it("(use, free, free) => error", () => {
     const result: Result = free(free(use(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, define, define", () => {
+  it("(free, define, define) => error", () => {
     const result: Result = define(define(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, define, use", () => {
+  it("(free, define, use) => error", () => {
     const result: Result = use(define(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, define, free", () => {
+  it("(free, define, free) => error", () => {
     const result: Result = free(define(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, use, define", () => {
+  it("(free, use, define) => error", () => {
     const result: Result = define(use(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, use, use", () => {
+  it("(free, use, use) => error", () => {
     const result: Result = use(use(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, use, free", () => {
+  it("(free, use, free) => error", () => {
     const result: Result = free(use(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, free, define", () => {
+  it("(free, free, define) => error", () => {
     const result: Result = define(free(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, free, use", () => {
+  it("(free, free, use) => error", () => {
     const result: Result = use(free(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
   });
 
-  it("free, free, free", () => {
+  it("(free, free, free) => error", () => {
     const result: Result = free(free(free(["ok", "pre"])));
     expect(is_ok(result)).toBe(false);
     expect(get_state(result)).toEqual("top");
