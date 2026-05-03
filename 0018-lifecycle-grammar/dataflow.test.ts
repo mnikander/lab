0bc4d1dbd5aa360087a22f384ed2c232ff2a930d
Marco Nikander <m4n1k.developer@gmail.com>
2026-05-03 16:17:11 +0200

    0018 a few more test-cases

--- 0018-lifecycle-grammar/dataflow.test.ts
+++ 0018-lifecycle-grammar/dataflow.test.ts
@@ -4,8 +4,8 @@ import { Program, Register } from "./grammar.ts";
 import { dataflow } from "./dataflow.ts";
 import { Result } from "./lattice.ts";
 
-describe("life-cycle", () => {
-  it("empty program", () => {
+describe("life-cycle for a single basic block", () => {
+  it("must accept an empty block", () => {
     const program: Program = [
       {
         name: "@main",
@@ -19,7 +19,27 @@ describe("life-cycle", () => {
     expect(maybe_error).toBe(undefined);
   });
 
-  it("simple program", () => {
+  it("must accept define", () => {
+    const program: Program = [
+      {
+        name: "@main",
+        params: [],
+        blocks: [
+          {
+            name: "@entry",
+            lines: [
+              ["define", "%x"],
+            ],
+            terminator: ["return"],
+          },
+        ],
+      },
+    ];
+    const maybe_error: undefined | [Register, Result] = dataflow(program);
+    expect(maybe_error).toBe(undefined);
+  });
+
+  it("must accept define-use", () => {
     const program: Program = [
       {
         name: "@main",
@@ -40,7 +60,54 @@ describe("life-cycle", () => {
     expect(maybe_error).toBe(undefined);
   });
 
-  it("use-after-free", () => {
+  it("must accept define-use-free", () => {
+    const program: Program = [
+      {
+        name: "@main",
+        params: [],
+        blocks: [
+          {
+            name: "@entry",
+            lines: [
+              ["define", "%x"],
+              ["use", "%x"],
+              ["free", "%x"],
+            ],
+            terminator: ["return"],
+          },
+        ],
+      },
+    ];
+    const maybe_error: undefined | [Register, Result] = dataflow(program);
+    expect(maybe_error).toBe(undefined);
+  });
+
+  it("must reject use-before-define", () => {
+    const program: Program = [
+      {
+        name: "@main",
+        params: [],
+        blocks: [
+          {
+            name: "@entry",
+            lines: [
+              ["use", "%x"],
+              ["define", "%x"],
+            ],
+            terminator: ["return"],
+          },
+        ],
+      },
+    ];
+    const maybe_error: undefined | [Register, Result] = dataflow(program);
+    expect(maybe_error).toEqual(["%x", [
+      "error",
+      "top",
+      "use-before-define",
+    ]]);
+  });
+
+  it("must reject use-after-free", () => {
     const program: Program = [
       {
         name: "@main",
@@ -67,7 +134,32 @@ describe("life-cycle", () => {
     ]]);
   });
 
-  it("double-free", () => {
+  it("must reject double-define", () => {
+    const program: Program = [
+      {
+        name: "@main",
+        params: [],
+        blocks: [
+          {
+            name: "@entry",
+            lines: [
+              ["define", "%x"],
+              ["define", "%x"],
+            ],
+            terminator: ["return"],
+          },
+        ],
+      },
+    ];
+    const maybe_error: undefined | [Register, Result] = dataflow(program);
+    expect(maybe_error).toEqual(["%x", [
+      "error",
+      "top",
+      "define-after-define",
+    ]]);
+  });
+
+  it("must reject double-free", () => {
     const program: Program = [
       {
         name: "@main",
