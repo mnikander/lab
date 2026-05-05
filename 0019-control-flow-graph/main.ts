9059fba99ee2150695f29f4fbd762daeff5710ec
Marco Nikander <m4n1k.developer@gmail.com>
2026-05-05 21:24:02 +0200

    0019 define control flow graph type

--- 0019-control-flow-graph/main.ts
+++ 0019-control-flow-graph/main.ts
@@ -1,1 +1,11 @@
 // Copyright (c) 2026 Marco Nikander
+
+import * as HIGH from "./grammar.ts";
+
+export type Node = {
+  name: HIGH.Label;
+  predecessors: number[];
+  successors: number[];
+};
+
+export type CFG = Node[];
